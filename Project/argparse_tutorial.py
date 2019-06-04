517d19351a936c8144a8da6a1297ab10aaca860c
dasaed <carlos.dasaed@gmail.com>
2019-06-04 11:14:51 -0230

    Tue Jun  4 11:14:51 NDT 2019: quick modification

--- Project/argparse_tutorial.py
+++ Project/argparse_tutorial.py
@@ -1,8 +1,9 @@
 import argparse
 parser = argparse.ArgumentParser()
 # List of arguments #
+parser.add_argument("x", type=int, help="The base number")
 parser.add_argument("square", type=int, help="display square of a given number")
-parser.add_argument("-v", "--verbosity", help="increases output verbosity", type=int)
+parser.add_argument("-v", "--verbosity", help="increases output verbosity", action="count",default=0)
 
 # With the following option, verbosity doesn't need any arguments
 # parser.add_argument("-v", "--verbosity", help="increases output verbosity", action="store_true")
@@ -10,7 +11,7 @@ parser.add_argument("-v", "--verbosity", help="increases output verbosity", type
 args = parser.parse_args()
 answer = args.square**2
 
-if args.verbosity == 2:
+if args.verbosity >= 2:
     print("The square of {} equals {}".format(args.square,answer))
 elif args.verbosity == 1:
     print("{}^2 = {}".format(args.square,answer))
