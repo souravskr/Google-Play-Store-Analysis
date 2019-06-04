81480e39232d118a70a309297f4601718af02b61
dasaed <carlos.dasaed@gmail.com>
2019-06-04 11:39:54 -0230

    dasaed-maclinux 06/04/19-11:39:54: quick modification

--- Project/argparse_tutorial.py
+++ Project/argparse_tutorial.py
@@ -1,19 +1,31 @@
+'''
+This little program was created using the argparse library tutorial,
+as found in the documentation at the following link
+https://docs.python.org/3/howto/argparse.html
+'''
 import argparse
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="This command calculates X to the power of Y", epilog="This is a very basic command")
+group = parser.add_mutually_exclusive_group()
 # List of arguments #
 parser.add_argument("x", type=int, help="The base number")
-parser.add_argument("square", type=int, help="display square of a given number")
-parser.add_argument("-v", "--verbosity", help="increases output verbosity", action="count",default=0)
+parser.add_argument("y", type=int, help="The exponent")
+
+# Group of mutuall exclusive options
+group.add_argument("-v", "--verbosity", help="increases output verbosity", action="count",default=0)
+group.add_argument("-q", "--quiet", help="decreases output verbosity",action="store_true" )
 
 # With the following option, verbosity doesn't need any arguments
 # parser.add_argument("-v", "--verbosity", help="increases output verbosity", action="store_true")
 
 args = parser.parse_args()
-answer = args.square**2
+answer = args.x ** args.y
 
-if args.verbosity >= 2:
-    print("The square of {} equals {}".format(args.square,answer))
+if args.verbosity >= 3:
+    print("Running '{}'".format(__file__))
+    print("{} to the power of {} is {}".format(args.x,args.y, answer))
+elif args.verbosity == 2:
+    print("{} to the power of {} is {}".format(args.x,args.y, answer))
 elif args.verbosity == 1:
-    print("{}^2 = {}".format(args.square,answer))
-else:
+    print("{}^{} = {}".format(args.x,args.y,answer))
+elif args.quiet:
     print(answer)
