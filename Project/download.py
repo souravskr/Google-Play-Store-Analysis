1221b483d1665363836b9096787757faf80f6cae
dasaed <carlos.dasaed@gmail.com>
2019-06-04 13:16:19 -0230

    Downloading the files is done!

--- Project/download.py
+++ Project/download.py
@@ -48,27 +48,38 @@
 
 
 
-import argparse
-import requests
-import kaggle
-
-
-print("Downloading Kaggle Database")
-kaggle datasets download lava18/google-play-store-apps  --unzip -p /tmp
-
+#kaggle datasets download lava18/google-play-store-apps  --unzip -p /tmp
 
 ### Optional arguments ###
+import os
+import requests
+import kaggle
 import argparse
 parser = argparse.ArgumentParser(description="""\
 This is a simple script to download Kaggle datasets. 
 It is important to have a kaggle account for the script to work.
 
 """, epilog="Group 1 - CMSC6950 - Memorial University of Newfoundland")
-group = parser.add_mutually_exclusive_group()
+#group = parser.add_mutually_exclusive_group()
 
 
-parser.add_argument("-c", "--compressed", action="store_true", help="Downloads the compressed files from Kaggle")
-parser.add_argument("-o", "--output", action="store_true" , help="")
+parser.add_argument("-u", "--uncompressed", action="store_true", help="Downloads the compressed files from Kaggle")
+parser.add_argument("-o", "--output", help="select the output directory for the files", default=".")
+args = parser.parse_args()
+
+odirectory = args.output
+
+if args.uncompressed:
+    os.system('kaggle datasets download lava18/google-play-store-apps --unzip -p '+odirectory)
+else:
+    os.system('kaggle datasets download lava18/google-play-store-apps -p '+odirectory)
+    #!kaggle datasets download lava18/google-play-store-apps -p args.o
+
+
+
+
+
+
 
 
 
