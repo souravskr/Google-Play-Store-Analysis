1c965e22d7b1427baf04750a63bde7ddae933744
souravs <souravs@mun.ca>
2019-06-10 18:19:36 -0230

    This is test 2

--- Project/cleanup.py
+++ Project/cleanup.py
@@ -31,3 +31,7 @@ print(data1)
 #data3 = data1.count(axis=1)
 #print(data3.count())
 ###data1['Last Updated'] = df.date.apply( lambda x: pd.to_datetime(x).strftime('%m/%d/%Y')[0] )
+
+
+
+# This is another test comment !!!
