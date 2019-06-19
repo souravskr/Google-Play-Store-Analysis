31dd4a4648a2e9aaba5dc3021d66ea0ab08ef9ea
Aida Khosh Raftar Nouri <akhoshraftar@mun.ca>
2019-06-19 20:35:24 +0000

    Adding another interactive figure (Application Rating Vs Price of Applications)

--- Project/jitter.py
+++ Project/jitter.py
@@ -31,13 +31,27 @@ def plot_jitter(data_frame):
     output_file("./Data/jitter.html")
     show(jitter_plot)
     return True
+    
+def plot_jitter2(data_frame):
+    pricing = sorted(data_frame.price.unique())
+    jitter_plot =figure(plot_width=1600, plot_height=800,
+                                           title="Application Rating Vs Price of Applications", y_axis_type='log')
+
+    jitter_plot.xgrid.grid_line_color = "red"
+    jitter_plot.xaxis[0].ticker = pricing
+    jitter_plot.circle(x=jitter('price', 0.4), y='installs', size=9, alpha=0.4, source=data_frame,color="red")
+    jitter_plot.xaxis.axis_label = 'price'
+    jitter_plot.yaxis.axis_label = 'No. of Installations'
+    output_file("./Data/jitter2.html")
+    show(jitter_plot)
+    return True
 
 if __name__ == "__main__":
 
     try:
         # call file download process
         plot_jitter(read_file('./Data/cleanData.csv'))
-
+        plot_jitter2(read_file('./Data/cleanData.csv'))
 
     except Exception as e:
         print(e)
